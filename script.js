354b6dc38daf47ffe860eb6ad1942ba9afdea56c
palakthavrani <palak29iot045@satiengg.in>
2026-03-27 14:07:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+// Task add karne ka function
+function addTask() {
 
+    let input = document.getElementById("taskInput");
+    let task = input.value;
+
+    // agar input empty hai
+    if (task === "") {
+        alert("Please enter a task");
+        return;
+    }
+
+    let li = document.createElement("li");
+    li.innerText = task;
+
+    // delete button
+    let btn = document.createElement("button");
+    btn.innerText = "Delete";
+
+    btn.onclick = function () {
+        li.remove();
+    };
+
+    li.appendChild(btn);
+
+    document.getElementById("taskList").appendChild(li);
+
+    input.value = "";
+}
